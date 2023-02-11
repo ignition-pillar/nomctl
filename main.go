ee8723d4427e3cfee007582b1edd1b56fd50af24
georgezgeorgez <george@ignition.ooo>
2023-02-11 04:45:00 +0000

    Ensure .nomctl dir and add znn-cli flags

--- main.go
+++ main.go
@@ -4,23 +4,50 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/ignition-pillar/go-zdk/client"
 	"github.com/ignition-pillar/go-zdk/zdk"
 	"github.com/urfave/cli/v2"
 )
 
+func connect(url string, chainId int) (*zdk.Zdk, error) {
+	rpc, err := client.NewClient(url, client.ChainIdentifier(uint64(chainId)))
+	if err != nil {
+		return nil, err
+	}
+	z := zdk.NewZdk(rpc)
+	return z, nil
+}
+
 func main() {
-	url := "ws://127.0.0.1:35998"
-	rpc, err := client.NewClient(url)
+
+	home_dir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
-	z := zdk.NewZdk(rpc)
+	nomctl_dir := filepath.Join(home_dir, ".nomctl")
+	mode := int(0700)
+	err = os.MkdirAll(nomctl_dir, os.FileMode(mode))
+	if err != nil {
+		log.Fatal(err)
+	}
+	wallet_dir := filepath.Join(nomctl_dir, "wallet")
+	err = os.MkdirAll(wallet_dir, os.FileMode(mode))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var url string
+	var chainId int
 
 	znn_cli_frontierMomentum := &cli.Command{
 		Name: "frontierMomentum",
 		Action: func(cCtx *cli.Context) error {
+			z, err := connect(url, chainId)
+			if err != nil {
+				return err
+			}
 			m, err := z.Ledger.GetFrontierMomentum()
 			if err != nil {
 				return err
@@ -45,6 +72,44 @@ func main() {
 				Name:        "znn-cli",
 				Usage:       "A port of znn_cli_dart",
 				Subcommands: znn_cli_subcommands,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "url",
+						Aliases:     []string{"u"},
+						Usage:       "Provide a websocket znnd connection URL with a port",
+						Value:       "ws://127.0.0.1:35998",
+						Destination: &url,
+					},
+					&cli.IntFlag{
+						Name:        "chainId",
+						Aliases:     []string{"n"},
+						Usage:       "Specify the chain idendtifier to use",
+						Value:       1,
+						Destination: &chainId,
+					},
+					&cli.StringFlag{
+						Name:    "passphrase",
+						Aliases: []string{"p"},
+						Usage:   "use this passphrase for the keyStore or enter it manually in a secure way",
+					},
+					&cli.StringFlag{
+						Name:    "keyStore",
+						Aliases: []string{"k"},
+						Usage:   "Select the local keyStore",
+						Value:   "available keyStore if only one is present",
+					},
+					&cli.IntFlag{
+						Name:    "index",
+						Aliases: []string{"i"},
+						Usage:   "Address index",
+						Value:   0,
+					},
+					&cli.BoolFlag{
+						Name:    "verbose",
+						Aliases: []string{"v"},
+						Usage:   "Prints detailed information about the action that it performs",
+					},
+				},
 			},
 		},
 	}
