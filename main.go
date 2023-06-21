abd4f49d2cbd80f7c8e0049a7916e359631afaff
georgezgeorgez <george@ignition.ooo>
2023-06-21 21:17:46 +0000

    Add znn-cli wallet.createFromMnemonic and spork functions

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/tyler-smith/go-bip39"
 	"github.com/urfave/cli/v2"
 	"github.com/zenon-network/go-zenon/common/types"
+	"github.com/zenon-network/go-zenon/vm/constants"
 	"github.com/zenon-network/go-zenon/wallet"
 	//"github.com/faith/color"
 	// TODO color
@@ -209,6 +210,50 @@ func main() {
 			}
 
 			password := cCtx.Args().Get(0)
+			kf, _ := ks.Encrypt(password)
+			kf.Path = filepath.Join(walletDir, name)
+			//kf.Write()
+			// Uncomment when file mode is fixed
+			keyFileJson, err := json.MarshalIndent(kf, "", "    ")
+			if err != nil {
+				return err
+			}
+			os.WriteFile(kf.Path, keyFileJson, 0600)
+
+			fmt.Println("keyStore successfully created:", name)
+			return nil
+		},
+	}
+
+	znnCliWalletCreateFromMnemonic := &cli.Command{
+		Name:  "wallet.createFromMnemonic",
+		Usage: "passphrase [keyStoreName]",
+		Action: func(cCtx *cli.Context) error {
+			if !(cCtx.NArg() == 2 || cCtx.NArg() == 3) {
+				fmt.Println("Incorrect number of arguments. Expected:")
+				fmt.Println("wallet.createFromMnemonic \"mnemonic\" passphrase [keyStoreName]")
+				return nil
+			}
+
+			// TODO finally implement a local keystore manager in go-zdk?
+			ms := cCtx.Args().Get(0)
+            // TODO add in validation
+			entropy, _ := bip39.EntropyFromMnemonic(ms)
+			mnemonic, _ := bip39.NewMnemonic(entropy)
+			ks := &wallet.KeyStore{
+				Entropy:  entropy,
+				Seed:     bip39.NewSeed(mnemonic, ""),
+				Mnemonic: mnemonic,
+			}
+			_, kp, _ := ks.DeriveForIndexPath(0)
+			ks.BaseAddress = kp.Address
+
+			name := ks.BaseAddress.String()
+			if cCtx.NArg() == 3 {
+				name = cCtx.Args().Get(2)
+			}
+
+			password := cCtx.Args().Get(1)
 			kf, _ := ks.Encrypt(password)
 			kf.Path = filepath.Join(walletDir, name)
 			//kf.Write()
@@ -429,16 +474,151 @@ func main() {
 		},
 	}
 
+	znnCliSporkList := &cli.Command{
+		Name:  "spork.list",
+		Usage: "",
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.NArg() != 0 {
+				fmt.Println("Incorrect number of arguments. Expected:")
+				fmt.Println("spork.list")
+				return nil
+			}
+
+			z, err := connect(url, chainId)
+			if err != nil {
+				fmt.Println("Error connecting to Zenon Network:", err)
+				return err
+			}
+			sporkList, err := z.Embedded.Spork.GetAll(0, rpcMaxPageSize)
+			if err != nil {
+				fmt.Println("Error getting spork list:", err)
+				return err
+			}
+
+			if len(sporkList.List) == 0 {
+				fmt.Println("No sporks found")
+			} else {
+				fmt.Println("Sporks:")
+				for _, s := range sporkList.List {
+					fmt.Printf("Name: %v\n", s.Name)
+					fmt.Printf("  Description: %v\n", s.Description)
+					fmt.Printf("  Activated: %v\n", s.Activated)
+					if s.Activated {
+						fmt.Printf("  EnforcementHeight: %v\n", s.EnforcementHeight)
+					}
+					fmt.Printf("  Hash: %v\n", s.Id)
+				}
+			}
+
+			return nil
+		},
+	}
+
+	znnCliSporkCreate := &cli.Command{
+		Name:  "spork.create",
+		Usage: "name description",
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.NArg() != 2 {
+				fmt.Println("Incorrect number of arguments. Expected:")
+				fmt.Println("spork.list name description")
+				return nil
+			}
+
+			kp, err := getZnnCliSigner(walletDir, cCtx)
+			if err != nil {
+				fmt.Println("Error getting signer:", err)
+				return err
+			}
+
+			z, err := connect(url, chainId)
+			if err != nil {
+				fmt.Println("Error connecting to Zenon Network:", err)
+				return err
+			}
+
+			name := cCtx.Args().Get(0)
+			if len(name) < constants.SporkNameMinLength || len(name) > constants.SporkNameMaxLength {
+				fmt.Println("Spork name must be", constants.SporkNameMinLength, "to", constants.SporkNameMaxLength, "characters in length")
+				return nil
+			}
+			description := cCtx.Args().Get(1)
+			if len(description) > constants.SporkDescriptionMaxLength {
+				fmt.Println("Spork description cannot exceed", constants.SporkDescriptionMaxLength, "characters in length")
+			}
+
+			template, err := z.Embedded.Spork.Create(name, description)
+			if err != nil {
+				fmt.Println("Error templating spork create tx:", err)
+				return err
+			}
+			fmt.Println("Creating spork...")
+			_, err = utils.Send(z, template, kp, false)
+			if err != nil {
+				fmt.Println("Error sending spork create tx:", err)
+				return err
+			}
+
+			fmt.Println("Done")
+			return nil
+		},
+	}
+
+	znnCliSporkActivate := &cli.Command{
+		Name:  "spork.activate",
+		Usage: "id",
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.NArg() != 1 {
+				fmt.Println("Incorrect number of arguments. Expected:")
+				fmt.Println("spork.activate id")
+				return nil
+			}
+
+			kp, err := getZnnCliSigner(walletDir, cCtx)
+			if err != nil {
+				fmt.Println("Error getting signer:", err)
+				return err
+			}
+
+			z, err := connect(url, chainId)
+			if err != nil {
+				fmt.Println("Error connecting to Zenon Network:", err)
+				return err
+			}
+
+			id := types.HexToHashPanic(cCtx.Args().Get(0))
+
+			template, err := z.Embedded.Spork.Activate(id)
+			if err != nil {
+				fmt.Println("Error templating spork activate tx:", err)
+				return err
+			}
+			fmt.Println("Activating spork...")
+			_, err = utils.Send(z, template, kp, false)
+			if err != nil {
+				fmt.Println("Error sending spork activate tx:", err)
+				return err
+			}
+
+			fmt.Println("Done")
+			return nil
+		},
+	}
+
 	znnCliSubcommands := []*cli.Command{
 		znnCliBalance,
 		znnCliFrontierMomentum,
 		znnCliWalletCreateNew,
+		znnCliWalletCreateFromMnemonic,
 		znnCliWalletList,
+		//		znnCliWalletDeriveAddresses,
 		znnCliPlasmaGet,
 		znnCliPillarList,
 		znnCliPillarCollect,
 		znnCliPillarDelegate,
 		znnCliPillarUndelegate,
+		znnCliSporkList,
+		znnCliSporkCreate,
+		znnCliSporkActivate,
 	}
 
 	utilsValidateAddress := &cli.Command{
